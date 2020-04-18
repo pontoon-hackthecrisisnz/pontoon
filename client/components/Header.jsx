0656609e04606d7be91f0475b4e1d1606c800825
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2020-04-19 10:19:41 +1200

    style component home

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,9 +4,9 @@ import { Container } from 'reactstrap';
 const Header = () => {
   return (
     <div className="home-page-header">
-      <Container className="home-page-headline text-center">
-        <h1>PONTOON</h1>
-      </Container>
+      <div className="home-page-headline text-center">
+        <img src="/images/Pontoon.png" alt=""/>
+      </div>
     </div>
   );
 };
