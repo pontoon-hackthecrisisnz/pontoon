ffb27096aa1719ed8b3feead6551efc1f51c701c
Ethan Meleah <ethan.meleah@gmail.com>
2020-04-18 14:44:45 +1200

    redux-pass-over

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Home from './Home';
+import Register from './Register';
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Register />
     </>
   );
 };
--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 class Register extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            name: '',
+            region: '',
+            town: '',
+            //lastJobs,
+            minSalary: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submit working as intended.')
     }
 
     render() {
         return(
-            <p> Register. </p>
+            <div className="register">
+                <h1>{this.props.user.test}</h1>
+                <form>
+                    <input type='text' placeholder='name' name='name' onChange={this.handleChange} />
+                    <input type='text' placeholder='region' name='region' onChange={this.handleChange} />
+                    <input type='text' placeholder='town' name='town' onChange={this.handleChange} />
+                    <input type='text' placeholder='minimum salary' name='minSalary' onChange={this.handleChange} />
+
+                    <input  type="submit" onClick={this.handleSubmit} />
+                </form>
+            </div>
         )
     }
 }
 
-export default Register
+function mapStateToProps(state) {
+    return {user: state.user}
+}
+
+export default connect(mapStateToProps)(Register)
+
+
