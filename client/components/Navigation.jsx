4a9bec29af87e317463fb72f917e7b4496423d55
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2020-04-21 23:12:08 +1200

    fix styling of search inputs

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -4,13 +4,13 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 const Navigation = () => {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="rgb(32, 40, 41)" variant="dark">
         <Navbar.Brand href="#"><h1>Pontoon</h1></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse className="responsive-navbar-nav" className='nav-layout'>
-          <Nav className="mr-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+        <Navbar.Collapse className="responsive-navbar-nav">
+          <Nav className="mr-auto justify-content-right"> 
+            <Nav.Link href="#about"><h3>About</h3></Nav.Link>
+            <Nav.Link href="#contact"><h3>Contact</h3></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
